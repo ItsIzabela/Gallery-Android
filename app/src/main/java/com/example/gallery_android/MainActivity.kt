9b06d8acb6cbd431bed8ad0419e620f69551b448
Izabela Majcherczyk <xeclipse.dev@gmail.com>
2026-05-25 11:47:26 +0200

    feat: open gallery button working

--- app/src/main/java/com/example/gallery_android/MainActivity.kt
+++ app/src/main/java/com/example/gallery_android/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.gallery_android
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.example.gallery_android.ui.theme.GalleryAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GalleryAndroidTheme {
-                Greeting()
+                ImagePickerScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting() {
-    Text(
-        text = "Hello Gallery!",
-        color = MaterialTheme.colorScheme.onPrimary,
+fun ImagePickerScreen() {
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    // Launcher do wybierania obrazu z urządzenia
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primary)
-    )
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(onClick = { launcher.launch("image/*") }) {
+            Text("Wybierz obraz z urządzenia")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        imageUri?.let { uri ->
+            Image(
+                painter = rememberAsyncImagePainter(uri),
+                contentDescription = "Wybrany obraz",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewImagePicker() {
     GalleryAndroidTheme {
-        Greeting()
+        ImagePickerScreen()
     }
 }
